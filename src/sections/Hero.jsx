1d972ef915a6e063ddcac3e754f5a7e9df8c19b6
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2026-07-15 18:28:34 +0200

    feat: add hero heading

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return(
+        <section>
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+                Hi, I am Olena <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">Building Websites...</p>
+            </div>
+        </section>
+    )
+}
+export default Hero;
