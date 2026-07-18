905ee09cfe99be938ab50acdd8656ef6e67d1808
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2026-07-18 19:01:17 +0200

    change settings for 3d object position

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,54 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Room from "../components/Room";
 import CanvasLoader from "../components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from "../constans";
 
 const Hero = () => {
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    // const x = useControls('Room', {
+    //     positionX: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max:10
+    //     },
+    //     positionY: {
+    //         value:2.5,
+    //         min:-10,
+    //         max:10
+    //     },
+    //      positionZ: {
+    //         value:2.5,
+    //         min:-10,
+    //         max:10
+    //     },
+    //     rotationX: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max:10
+    //     },
+    //     rotationY: {
+    //         value:2.5,
+    //         min:-10,
+    //         max:10
+    //     },
+    //      rotationZ: {
+    //         value:2.5,
+    //         min:-10,
+    //         max:10
+    //     },
+    //     scale: {
+    //         value: 1,
+    //         min: 0.1,
+    //         max:10
+    //     }
+    // })
     return(
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -14,13 +60,19 @@ const Hero = () => {
                 <p className="hero_tag text-gray_gradient">Building Websites...</p>
             </div>
             <div className="w-full h-full absolute inset-0">
+                {/* <Leva/> */}
                 {/* 3d started */}
                 <Canvas className="w-full h-full">
                     {/* when room is loading  */}
                     <Suspense fallback={<CanvasLoader/>}>        
-                        <PerspectiveCamera makeDefault position={[0,0,30]}/>
-                        <Room scale={0.05} position={[0,0,0]} rotation={[0, -Math.PI/2,0]}/>
-                        <ambientLight intensity={1} />
+                        <PerspectiveCamera makeDefault position={[0,0,20]}/>
+                        <Room 
+                            // scale={0.07} 
+                            position={sizes.deskPosition}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={sizes.deskScale}
+                        />
+                        <ambientLight intensity={0.5} />
                         <directionalLight position={[10, 10, 10]} intensity={0.5} />                    
                     </Suspense>
                 </Canvas>
