bdcc244cf7c677e50f3f3ba9137deb4c1bb87a31
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2026-08-15 10:58:28 +0200

    add about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './sections/Navbar.jsx';
 import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <main className="max-w-7xl mx-auto relative">
       <Navbar/>
       <Hero/>
+      <About/>
     </main>
   )
 }
