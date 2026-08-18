8e6e1724a2055f926a448a425e76b269042cf2af
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2026-08-18 11:47:27 +0200

    delete console log

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -19,9 +19,6 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
-console.log(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
